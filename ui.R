b787b992104fddc642aa5cf394d78962ff31ec2c
cstanca1 <cstanca1@yahoo.com>
2016-02-28 18:00:27 -0500

    first commit

---
+++ ui.R
@@ -0,0 +1,61 @@
+library(shiny)
+
+shinyUI(navbarPage(
+  "Population and Economic Impact of Natural Disasters in USA",
+  tabPanel(
+    "Plot",
+    sidebarPanel(
+      sliderInput(
+        "range",
+        "Year Range:",
+        min = 2000,
+        max = 2010,
+        value = c(2000, 2010),
+        sep = ","
+      ),
+      uiOutput("evtypeControls")
+    ),
+    
+    mainPanel(tabsetPanel(
+ 
+      tabPanel(
+        'By State',
+        column(3,
+               wellPanel(
+                 radioButtons(
+                   "populationImpactCategory",
+                   "Population:",
+                   c(
+                     "Both" = "both", "Injuries" = "injuries", "Fatalities" = "fatalities"
+                   )
+                 )
+               )),
+        column(3,
+               wellPanel(
+                 radioButtons(
+                   "economicImpactCategory",
+                   "Economic:",
+                   c(
+                     "Both" = "both", "Property" = "property", "Crops" = "crops"
+                   )
+                 )
+               )),
+        column(
+          7,
+          plotOutput("populationImpactByState"),
+          plotOutput("economicImpactByState")
+        )
+      ),
+	  
+	  tabPanel(
+        'By Year',
+        h4('Events by Year', align = "center"),
+        showOutput("eventsByYear", "nvd3"),
+        h4('Population Impact by Year', align = "center"),
+        showOutput("populationImpact", "nvd3"),
+        h4('Economic Impact by Year', align = "center"),
+        showOutput("economicImpact", "nvd3")
+      )	  
+  )
+)
+)))
